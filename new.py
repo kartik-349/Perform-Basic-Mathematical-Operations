1046bca7d8e4bcfd0e68d2a7ab4410f014a53d5d
kartik-349 <112315966+kartik-349@users.noreply.github.com>
2025-05-16 21:12:50 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,18 @@
+a = input("Enter the first number : ")
+b = input("Enter the second number : ")
+a = int(a)
+b = int(b)
+
+Add = a + b
+print("Addition", Add)
+
+Subs = a - b
+print("Substraction", Subs)
+
+Mul = a * b
+print("Multiplication", Mul)
+
+Div = a / b
+Div=float(Div)
+print("Divison", Div)
+
